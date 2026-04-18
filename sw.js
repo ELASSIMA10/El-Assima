99de868628f4031cc6ad52fe485c3fa9f5a523cb
Admin <admin@nabil-carte.com>
2026-04-18 16:53:58 +0200

    Ajout du PWA (Manifest et Service Worker)

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'el-assima-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/ELASSIMA_HUB.html',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => console.log('Fetch failed, user might be offline.'));
+      })
+  );
+});
